1ecca0de1b60cd410950d82a2e9398053265c6b6
Sidharth Kumar <sidharthkumar@Sidharths-MacBook-Pro.local>
2023-04-05 02:25:41 +0530

    Another Commit

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://taskapp:taskappmf@cluster0.fezwscf.mongodb.net/?retryWrites=true&w=majority', {    // Here the mongoose npm module connects with the mongoDB and creates a database named 'task-manager-api'
+console.log(process.env.MONGODB_URL);
+mongoose.connect(process.env.MONGODB_URL, {    // Here the mongoose npm module connects with the mongoDB and creates a database named 'task-manager-api'
     useNewUrlParser: true,
     useCreateIndex:true,
     useFindAndModify : false,   // Used to avoid the deprecation warning that pops up when we use the 'findByIdAndUpdate(...)' method
